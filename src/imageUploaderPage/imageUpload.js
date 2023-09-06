ce1f0959b5a1e03c8013afd15d1039d5d7464a65
Vineet2012 <panwarvineet2012@gmail.com>
2023-09-06 13:47:06 +0530

    Design changed

--- src/imageUploaderPage/imageUpload.js
+++ src/imageUploaderPage/imageUpload.js
@@ -8,16 +8,16 @@ import "./imageUploader.css";
 
 export default function ImageUploadCmp() {
   const inputRef = useRef(null);
-  const [loading , setLoading] = useState(false);
-  const [image , setImage] = useState(null);
-  const [text , setCropText] = useState("");
-  const [Fulltext , setFullText] = useState("");
-  const [progress , setProgress] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState(null);
+  const [text, setCropText] = useState("");
+  const [Fulltext, setFullText] = useState("");
+  const [progress, setProgress] = useState(0);
   const [filled, setFilled] = useState(100);
-  const [isRunning , setIsRunning] = useState(false);
+  const [isRunning, setIsRunning] = useState(false);
   const [src, setSrc] = useState("");
-  const [crop , setCrop] = useState({aspect: 1/1});
-  const [cropedImage , setCropedImage] = useState(null);
+  const [crop, setCrop] = useState({ aspect: 1 / 1 });
+  const [cropedImage, setCropedImage] = useState(null);
   const [imageRef, setImageRef] = useState(null);
   const [language, setLanguage] = useState({
     lang: "",
@@ -25,12 +25,12 @@ export default function ImageUploadCmp() {
 
 
   const handleChangeSelect = (event) => {
-    const {name , value} = event.target;
+    const { name, value } = event.target;
     setProgress(0);
     setLanguage(prevLang => {
       return {
         ...prevLang,
-        [name] : value
+        [name]: value
       }
     })
   }
@@ -46,242 +46,256 @@ export default function ImageUploadCmp() {
 
   const handleClickCrop = () => {
     setLoading(true);
-      setIsRunning(true);
-    if(cropedImage === null) {
+    setIsRunning(true);
+    if (cropedImage === null) {
       alert("CROP THE IMAGE FIRST");
-    }else {
-      if(language.lang === 'Hindi') {
-      Tesseract.recognize(
-        cropedImage,
-        "hin",
-        { logger: m => {console.log(m);
-        if(m.status === "recognizing text") {
-          setProgress(parseInt(m.progress * 100))
-        }
-        } }
-      ).then(({ data: { text } }) => {
-  
+    } else {
+      if (language.lang === 'Hindi') {
+        Tesseract.recognize(
+          cropedImage,
+          "hin",
+          {
+            logger: m => {
+              console.log(m);
+              if (m.status === "recognizing text") {
+                setProgress(parseInt(m.progress * 100))
+              }
+            }
+          }
+        ).then(({ data: { text } }) => {
+
           setCropText(text);
           setLoading(false);
-        
-      })
-    }else {
-      Tesseract.recognize(
-        cropedImage,
-        "eng",
-        { logger: m => {console.log(m);
-        if(m.status === "recognizing text") {
-          setProgress(parseInt(m.progress * 100))
-        }
-        } }
-      ).then(({ data: { text } }) => {
-  
+
+        })
+      } else {
+        Tesseract.recognize(
+          cropedImage,
+          "eng",
+          {
+            logger: m => {
+              console.log(m);
+              if (m.status === "recognizing text") {
+                setProgress(parseInt(m.progress * 100))
+              }
+            }
+          }
+        ).then(({ data: { text } }) => {
+
           setCropText(text);
           setLoading(false);
-        
-      })
+
+        })
+      }
     }
-  }
   }
 
   const handleClick = () => {
-      setLoading(true);
-      setIsRunning(true);
-      if(language.lang === 'Hindi') {
-        Tesseract.recognize(
-          image,
-          "hin",
-          { logger: m => {console.log(m);
-          if(m.status === "recognizing text") {
-            setProgress(parseInt(m.progress * 100))
+    setLoading(true);
+    setIsRunning(true);
+    if (language.lang === 'Hindi') {
+      Tesseract.recognize(
+        image,
+        "hin",
+        {
+          logger: m => {
+            console.log(m);
+            if (m.status === "recognizing text") {
+              setProgress(parseInt(m.progress * 100))
+            }
           }
-          } }
-        ).then(({ data: { text } }) => {
-    
-          setFullText(text);
-            setLoading(false);
-          
-        })
-      }else {
-        Tesseract.recognize(
-          image,
-          "eng",
-          { logger: m => {console.log(m);
-          if(m.status === "recognizing text") {
-            setProgress(parseInt(m.progress * 100))
+        }
+      ).then(({ data: { text } }) => {
+
+        setFullText(text);
+        setLoading(false);
+
+      })
+    } else {
+      Tesseract.recognize(
+        image,
+        "eng",
+        {
+          logger: m => {
+            console.log(m);
+            if (m.status === "recognizing text") {
+              setProgress(parseInt(m.progress * 100))
+            }
           }
-          } }
-        ).then(({ data: { text } }) => {
-    
-          setFullText(text);
-            setLoading(false);
-          
-        })
-      }
+        }
+      ).then(({ data: { text } }) => {
+
+        setFullText(text);
+        setLoading(false);
+
+      })
+    }
   }
 
   const handleDeleteCrop = () => {
     setIsRunning(false);
-      setLoading(false);
-      setCropText("");
-      setCropedImage(null);
-      setProgress(0);
-      setLanguage({lang: "English"})
+    setLoading(false);
+    setCropText("");
+    setCropedImage(null);
+    setProgress(0);
+    setLanguage({ lang: "English" })
   }
 
   const handleDeleteFull = () => {
     setIsRunning(false);
-      setLoading(false);
-      setFullText("");
-      setProgress(0);
-      setLanguage({lang: "English"})
+    setLoading(false);
+    setFullText("");
+    setProgress(0);
+    setLanguage({ lang: "English" })
   }
 
 
-    function getCroppedImg() {
-      const canvas = document.createElement('canvas');
-        const scaleX = imageRef?.naturalWidth / imageRef?.width;
-        const scaleY = imageRef?.naturalHeight / imageRef?.height;
-        canvas.width = crop.width;
-        canvas.height = crop.height;
-        const ctx = canvas.getContext('2d');
-      
-        const pixelRatio = window.devicePixelRatio;
-        canvas.width = crop.width * pixelRatio;
-        canvas.height = crop.height * pixelRatio;
-        ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
-        ctx.imageSmoothingQuality = 'high';
-      
-        ctx.drawImage(
-          imageRef,
-          crop.x * scaleX,
-          crop.y * scaleY,
-          crop.width * scaleX,
-          crop.height * scaleY,
-          0,
-          0,
-          crop.width,
-          crop.height,
-        );
+  function getCroppedImg() {
+    const canvas = document.createElement('canvas');
+    const scaleX = imageRef?.naturalWidth / imageRef?.width;
+    const scaleY = imageRef?.naturalHeight / imageRef?.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const ctx = canvas.getContext('2d');
 
-        if(cropedImage === null && canvas.width === 0) {
-          alert("FIRST CROP THE IMAGE FROM THE UPLOADED IMAGE \n USE THE MOUSE TO CROP THE IMAGE \n DOUBLE CLICK , HOLD AND CROP THE IMAGE")
-        }else {
-          const base64Image = canvas.toDataURL('image/jpeg');
-        setCropedImage(base64Image);
-        }
+    const pixelRatio = window.devicePixelRatio;
+    canvas.width = crop.width * pixelRatio;
+    canvas.height = crop.height * pixelRatio;
+    ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+    ctx.imageSmoothingQuality = 'high';
+
+    ctx.drawImage(
+      imageRef,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width,
+      crop.height,
+    );
+
+    if (cropedImage === null && canvas.width === 0) {
+      alert("FIRST CROP THE IMAGE FROM THE UPLOADED IMAGE \nUSE THE MOUSE TO CROP THE IMAGE \nDOUBLE CLICK , HOLD AND CROP THE IMAGE \nOR HOLD LEFT CLICK AND CROP THE IMAGE")
+    } else {
+      const base64Image = canvas.toDataURL('image/jpeg');
+      setCropedImage(base64Image);
     }
+  }
 
-   
-    
-  
+
+
+
 
   return (
     <div>
       <NavBar />
-    
-    <div className='image-wrapper'>
-      <input type='file' ref={inputRef} style={{display: "none"}} onChange={handleImageChange} />
-      {/* after image uploadation starts */}
-      <div className='image-uploader-wrapper'>
-      {image ?
-      <div className='upload-wrapper'>
-      {/* <span className='select-upload-text'>SELECT AND UPLOAD THE IMAGE</span> */}
 
-      <div className="image-outer-wrapper">
-      <ReactCrop
-    crop={crop}
-    onChange={setCrop}
->
-    <img
-    style={{marginTop: "20px"}}
-       src={image}
-       onComplete={setSrc}
-       onLoad={(e) => {
-        setImageRef(e.target);
-       }}
-    />
-</ReactCrop>
-</div>
-<div className="buttons-outer-wrapper">
-      <div className='upload-convert-wrapper'>
-      <button className="upload-button" onClick={getCroppedImg}>Crop</button>
-      <>
-      </>
-      <button onClick={handleClickEvent} className='upload-button'>UPLOAD(Upload new image)</button>
-      <button onClick={handleClickCrop} className='upload-button'>Cropped Image (Convert to text)</button>
-      <button onClick={handleClick} className='upload-button'>Convert whole Image to text</button>
-      </div>
-      
-      <select 
-              className="selectTag"
-              id="lang"
-              value={language.lang}
-              onChange={handleChangeSelect}
-              name="lang"
-      >
-        <option value="English">English</option>
-        <option value="Hindi">Hindi</option>
-      </select>
-      </div>
-      {/* after image uploadation ends */}
-      </div>
-      : 
-      // before image upload starts
-      <div className='before-uploadation-wrapper' >
-        <span className='select-upload-text'>SELECT AND UPLOAD THE IMAGE</span>
-      <button onClick={handleClickEvent} className='after-upload-delete-button'>UPLOAD</button>
-      </div>
-      // before image upload ends
-  }
-  {loading &&  (
-    // progress bar starts
-     <div className='progressBar'>
-            <div style={{
-              height:"100%",
-              width: `${filled}%`,
-              backgroundColor: "#1976d2",
-              transition: "width 0.5s"
-            }}></div>
-            <span className="progressPercentage">{progress}%</span>
-        </div> 
-        // progress bar ends
-  )}
-  
-      </div>
-      {/* image cropping starts */}
-      {cropedImage &&
-      <div className="croped-image-text-wrapper">
-      <div className="cropped-text-wrapper">
-        <div className="cropped-text">
-      <div style={{display: "flex", flexDirection: "column"}}>
-      <span className="cropped-image-full-text">Cropped Image</span>
-      <img className="cropped-image" src={cropedImage}  />
-      </div>
-      </div>
-      <div className="cropped-text">
-      {!loading && text && (
-        <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
-      <textarea className="textarea-component"  value={text} onChange={(e) => setCropText(e.target.value)} rows={10}></textarea>
-      </div>
-      )}
-      </div>
-  </div>
-  {cropedImage  && <button onClick={handleDeleteCrop} className="after-upload-delete-button">Delete</button>}
-  </div>
-    }
-  {/* image cropping ends */}
-  {/* whoke image to text starts */}
-  {image && !loading && Fulltext && 
-  <div className="full-text-image-wrapper">
-  <span className="cropped-image-full-text">Uploaded Image-Text Conversion</span>
-   <textarea className="textarea-component" value={Fulltext} onChange={(e) => setFullText(e.target.value)} rows={10}></textarea> 
-   <button onClick={handleDeleteFull} className="after-upload-delete-button">Delete</button>
-   </div>
+      <div className='image-wrapper'>
+        <input type='file' ref={inputRef} style={{ display: "none" }} onChange={handleImageChange} />
+        {/* after image uploadation starts */}
+        <div className='image-uploader-wrapper'>
+          {image ?
+            <div className='upload-wrapper'>
+              {/* <span className='select-upload-text'>SELECT AND UPLOAD THE IMAGE</span> */}
 
-  }
-  {/* whoke image to text ends */}
-    </div>
+              <div className="image-outer-wrapper">
+                <div className="uploaded-image-wrapper">
+                  <ReactCrop
+                    crop={crop}
+                    onChange={setCrop}
+                  >
+                    <img
+                      style={{ width: "100%", height: "100%" }}
+                      src={image}
+                      onComplete={setSrc}
+                      onLoad={(e) => {
+                        setImageRef(e.target);
+                      }}
+                    />
+                  </ReactCrop>
+                </div>
+              </div>
+              <div className="buttons-outer-wrapper">
+                <div className='upload-convert-wrapper'>
+                  <button className="upload-button" onClick={getCroppedImg}>Crop</button>
+                  <>
+                  </>
+                  <button onClick={handleClickEvent} className='upload-button'>UPLOAD(Upload new image)</button>
+                  <button onClick={handleClickCrop} className='upload-button'>Cropped Image (Convert to text)</button>
+                  <button onClick={handleClick} className='upload-button'>Convert whole Image to text</button>
+                </div>
+
+                <select
+                  className="selectTag"
+                  id="lang"
+                  value={language.lang}
+                  onChange={handleChangeSelect}
+                  name="lang"
+                >
+                  <option value="English">English</option>
+                  <option value="Hindi">Hindi</option>
+                </select>
+              </div>
+              {/* after image uploadation ends */}
+            </div>
+            :
+            // before image upload starts
+            <div className='before-uploadation-wrapper' >
+              <span className='select-upload-text'>SELECT AND UPLOAD THE IMAGE</span>
+              <button onClick={handleClickEvent} className='after-upload-delete-button'>UPLOAD</button>
+            </div>
+            // before image upload ends
+          }
+          {loading && (
+            // progress bar starts
+            <div className='progressBar'>
+              <div style={{
+                height: "100%",
+                width: `${filled}%`,
+                backgroundColor: "#5963fb",
+                transition: "width 0.5s"
+              }}></div>
+              <span className="progressPercentage">{progress}%</span>
+            </div>
+            // progress bar ends
+          )}
+
+        </div>
+        {/* image cropping starts */}
+
+        {/* image cropping ends */}
+        {/* whoke image to text starts */}
+        {image && !loading && Fulltext &&
+          <div className="full-text-image-wrapper">
+            <div className="fullimg-text">
+              <div className="full-image-wrapper">
+                <img className="full-img-conversion" src={image} />
+              </div>
+              {/* <span className="cropped-image-full-text">Uploaded Image-Text Conversion</span> */}
+              <textarea className="textarea-component2" placeholder="Your text will apppear here" value={Fulltext} onChange={(e) => setFullText(e.target.value)} rows={10}></textarea>
+            </div>
+            <div></div>
+            <button onClick={handleDeleteFull} className="after-upload-delete-button">Delete</button>
+          </div>
+        }
+        <div style={{ marginTop: "8%" }}></div>
+        {/* whoke image to text ends */}
+        {cropedImage &&
+
+          <div className="croped-image-text-wrapper">
+            <div className="fullimg-text1">
+              <div className="cropped-img">
+                <span className="cropped-image-full-text">Cropped Image</span>
+                <img className="cropped-image" src={cropedImage} />
+              </div>
+              <textarea className="textarea-component1" placeholder="Your cropped image text will appear here" value={text} onChange={(e) => setCropText(e.target.value)} rows={10}></textarea>
+            </div>
+            <button onClick={handleDeleteCrop} className="after-upload-delete-button">Delete</button>
+          </div>
+        }
+      </div>
     </div>
   )
 }
--- src/navbar/navBar.js
+++ src/navbar/navBar.js
@@ -7,12 +7,13 @@ export default function NavBar() {
       <div className='nav-bar'>
         <span className='nav-title'> ClickBoard</span>
         <span className='nav-title'>IMAGE READER</span>
-        <div style={{display: "flex"}}>
-        <button className='signIn-button'>Sign In</button>
-        <button className='signUp-button'>Sign Up</button>
+        <div style={{ display: "flex" }}>
+          <button className='signIn-button'>Sign In</button>
+          <button className='signUp-button'>Sign Up</button>
+        </div>
       </div>
-      </div>
-      <span className='company-name'>ClickBoard Easy Uploadation</span>
+      <span className='company-name'>Image to Text Converter</span>
+      <span className='company-name-text'>Convert image to text in seconds with ClickBoard. Trusted by 1000 enterprises to extract text from images online.</span>
     </div>
   )
 }
